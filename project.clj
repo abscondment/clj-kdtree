899056e5ae8de0f59cdad3fb51ca7b4eb9d027fa
abscondment <brendan.ribera@gmail.com>
2009-12-03 10:59:23 -0800

    Neglected to add this...

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject clj-kdtree "0.1"
+  :dependencies [[org.clojure/clojure
+                  "1.1.0-alpha-SNAPSHOT"]
+                 [org.clojure/clojure-contrib
+                  "1.0-SNAPSHOT"]])
