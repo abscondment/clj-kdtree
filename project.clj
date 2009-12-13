faf645104370e6f59aec5e477768fd70ca6f42c2
abscondment <brendan.ribera@gmail.com>
2009-12-12 16:01:12 -0800

    Transitioning deps from clojure-alpha to clojure-master.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject clj-kdtree "0.1"
-  :dependencies [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.1.0-master-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.0-SNAPSHOT"]]
   :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]])
