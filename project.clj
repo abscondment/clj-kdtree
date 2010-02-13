96ab350f2886eb43d4f4b756812fddbe4d811184
abscondment <brendan.ribera@gmail.com>
2010-02-12 16:00:44 -0800

    Adding swank as a dev dependency, specifying clojure & contrib versions as a range.

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject clj-kdtree "0.1"
-  :dependencies [[org.clojure/clojure "1.1.0-master-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.1.0-master-SNAPSHOT"]]
-  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]])
+  :dependencies [[org.clojure/clojure "[1.0,1.2.0]"]
+                 [org.clojure/clojure-contrib "[1.0,1.2.0]"]]
+  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
+                     [leiningen/lein-swank "1.1.0"]])
