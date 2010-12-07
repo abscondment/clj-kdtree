ba9c23fe3235f356ddbb400f92d21a97837e7dd4
abscondment <brendan.ribera@gmail.com>
2010-12-07 09:30:05 -0800

    Sometimes it's nice to have contrib methods around for testing/development, but they're not required to use the library.

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject clj-kdtree "0.1.1"
   :description "kd-trees for Clojure"
   :dependencies [[org.clojure/clojure "[1.0.0,)"]]
-  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]])
+  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]
+                     [org.clojure/clojure-contrib "[1.0.0,)"]])
