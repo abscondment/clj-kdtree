883efefdb1edc8bd6a1f575b649cccfcf3ddcfee
abscondment <brendan.ribera@gmail.com>
2012-05-19 22:10:22 -0700

    Include Clojure 1.4 and move over to the lein-swank plugin.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject clj-kdtree "1.0.1"
   :description "kd-trees for Clojure"
-  :dependencies [[org.clojure/clojure "[1.2.0,1.3.0]"]]
-  :dev-dependencies [[swank-clojure "1.3.4-SNAPSHOT"]])
+  :dependencies [[org.clojure/clojure "[1.2.0,1.4.0]"]]
+  :plugins [[lein-swank "1.4.4"]])
