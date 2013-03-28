4050d330827b71cf241be4bace53610414877b0b
Brendan Ribera <brendan.ribera@gmail.com>
2013-03-28 11:32:44 -0700

    Preparing to release point metadata.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-kdtree "1.0.1"
+(defproject clj-kdtree "1.1.0"
   :description "kd-trees for Clojure"
   :dependencies [[org.clojure/clojure "[1.2.0,1.4.0]"]]
   :plugins [[lein-swank "1.4.4"]])
