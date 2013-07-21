b468fdc475d147668289839204637cd1ccf727da
abscondment <brendan.ribera@gmail.com>
2013-07-21 14:11:03 -0700

    Clojure 1.5 passes tests.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject clj-kdtree "1.1.0"
   :description "kd-trees for Clojure"
-  :dependencies [[org.clojure/clojure "[1.2.0,1.4.0]"]]
+  :dependencies [[org.clojure/clojure "[1.2.0,1.5.0]"]]
   :plugins [[lein-swank "1.4.4"]])
