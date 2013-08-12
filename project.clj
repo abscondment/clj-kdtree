966121c7af75d116dda007197179f94955a40f43
abscondment <brendan.ribera@gmail.com>
2013-08-12 13:30:29 -0700

    Add license & url to project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,8 @@
 (defproject clj-kdtree "1.1.1"
   :description "kd-trees for Clojure"
+  :url "https://github.com/abscondment/clj-kdtree"
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/MIT"
+            :distribution :repo}
   :dependencies [[org.clojure/clojure "[1.2.0,1.5.0]"]]
   :plugins [[lein-swank "1.4.4"]])
